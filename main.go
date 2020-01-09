bb0eff8310a0db87058a75aa7980cebe984692e4
Renato Nascimento <renato.nascimento@inlocomedia.com>
2020-01-09 19:29:16 -0300

    feat(adr): add new command

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"gopkg.in/alecthomas/kingpin.v2"
 
+	"github.com/inloco/adr-tool/new"
 	newTeam "github.com/inloco/adr-tool/newteam"
 )
 
@@ -13,11 +14,17 @@ var (
 
 	newTeamCmd  = cli.Command("new-team", "Create a new directory with a copy of the template")
 	newTeamCode = newTeamCmd.Arg("code", "Code that identifies your team. Max 6 chars.").Required().String()
+
+	newCmd      = cli.Command("new", "Create a new ADR")
+	newCmdTeam  = newCmd.Flag("team", "Code that identifies your team.").String()
+	newCmdTitle = newCmd.Arg("title", "Title of your new ADR.").Required().String()
 )
 
 func main() {
 	switch kingpin.MustParse(cli.Parse(os.Args[1:])) {
 	case newTeamCmd.FullCommand():
 		newTeam.NewTeam(*newTeamCode)
+	case newCmd.FullCommand():
+		new.New(*newCmdTeam, *newCmdTitle)
 	}
 }
---
+++ new/new.go
@@ -0,0 +1,98 @@
+package new
+
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strconv"
+	"time"
+
+	"github.com/Machiel/slugify"
+)
+
+const (
+	newADRFormat       = "%s/%s-%s.md"
+	templateFileFormat = "%s/template.md"
+
+	titleRegex  = "^#[^#].*\n"
+	statusRegex = "\\*[ ]Status.*\\n"
+	dateRegex   = "\\*[ ]Date.*\\n"
+
+	titleFormat  = "# %s\n"
+	statusFormat = "* Status: proposed\n"
+	dateFormat   = "* Date: 2006-01-02\n"
+)
+
+func extractNumber(s string) (int, error) {
+	re := regexp.MustCompile("^[0-9]+")
+	number := re.FindString(s)
+
+	return strconv.Atoi(number)
+}
+
+func getNextADRNumber(team string) (string, error) {
+	files, err := ioutil.ReadDir(team)
+	if err != nil {
+		return "", err
+	}
+
+	highest := 0
+	for _, f := range files {
+		if i, err := extractNumber(f.Name()); err == nil && i > highest {
+			highest = i
+		}
+	}
+
+	number := highest + 1
+	paddedNumber := fmt.Sprintf("%04d", number)
+
+	return paddedNumber, nil
+}
+
+func getTemplate(team string) (string, error) {
+	path := fmt.Sprintf(templateFileFormat, team)
+
+	templateBytes, err := ioutil.ReadFile(path)
+	return string(templateBytes), err
+}
+
+func replaceInTemplate(template string, title string) string {
+	t := regexp.MustCompile(titleRegex)
+	mdTitle := fmt.Sprintf(titleFormat, title)
+	template = t.ReplaceAllString(template, mdTitle)
+
+	s := regexp.MustCompile(statusRegex)
+	mdStatus := statusFormat
+	template = s.ReplaceAllString(template, mdStatus)
+
+	d := regexp.MustCompile(dateRegex)
+	mdDate := time.Now().Format(dateFormat)
+	template = d.ReplaceAllString(template, mdDate)
+
+	return template
+}
+
+func New(team string, title string) {
+	slug := slugify.Slugify(title)
+	number, err := getNextADRNumber(team)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	path := fmt.Sprintf(newADRFormat, team, number, slug)
+
+	template, err := getTemplate(team)
+	if err != nil {
+		fmt.Printf("error getting template: %s\n", err)
+		return
+	}
+
+	adr := fmt.Sprintf(replaceInTemplate(template, title))
+
+	err = ioutil.WriteFile(path, []byte(adr), 0644)
+	if err != nil {
+		fmt.Printf("error writing new template: %s", err)
+		return
+	}
+}
